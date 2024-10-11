209bd527780591d18a5c10eb315d5bcc78cb6d5e
Saalim <ucmajmi@gmail.com>
2024-10-11 22:23:45 +0530

    First Spring Boot Commit

---
+++ src/test/java/MyFirstSpringBoot/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package MyFirstSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
